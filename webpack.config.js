eeb9fd2b980385a33e298e334d4de5d5b1886e05
mhari <michael_hari@apple.com>
2016-03-11 13:44:47 -0800

    (feat) Set up React 'Hello World' with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/application.js',
+  output: {
+    path: './',
+    filename: 'build.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  }
+};
